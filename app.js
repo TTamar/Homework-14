839344ec7731a21f6923f25a2c45777a23427c23
Tamar Putkaradze <99253555+TTamar@users.noreply.github.com>
2022-04-07 13:39:03 +0400

    add html and js files

---
+++ app.js
@@ -0,0 +1,32 @@
+//1
+
+let Numbers = [5, 10, 43, 67, 100];
+
+let SumNumbers = Numbers[0] + Numbers[1] + Numbers[2] + Numbers[3] + Numbers[4];
+
+// console.log(SumNumbers / Numbers.length);
+
+//2
+
+let Users = [
+  { name: "Tamari", age: 22 },
+  { name: "Mariami", age: 19 },
+  { name: "Nino", age: 25 },
+  { name: "Eka", age: 30 },
+  { name: "Anne", age: 17 },
+];
+
+//3
+
+let UserInfo = {
+  firstName: "Tamari",
+  lastName: "Putkaradze",
+  address: ["Address1", "Address2"],
+  age: 22,
+  phoneNumbers: [12223100, 234234234],
+};
+
+// 4
+
+// console.log("My name is"+" "+UserInfo.firstName+", My age is"+" "+UserInfo.age+", My address is"+" "+UserInfo.address[0]);
+// console.log(`My name is ${UserInfo.firstName}, My age is ${UserInfo.age}, My address is ${UserInfo.address[0]}`);
